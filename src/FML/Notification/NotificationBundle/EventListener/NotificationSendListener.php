8491abcd0c49933f00fcb895c7adf83bd15c6d1b
Patryk Szlagowski <patryksz@lsnova.pl>
2014-07-09 10:54:57 +0200

    notification bundle

---
+++ src/FML/Notification/NotificationBundle/EventListener/NotificationSendListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace FML\Notification\NotificationBundle\EventListener;
+
+use FML\Notification\NotificationBundle\Notification\Sender;
+use Symfony\Component\HttpKernel\Event\PostResponseEvent;
+
+/**
+ * We are listening to terminate event and send notifications
+ */
+class NotificationSendListener {
+
+    /**
+     * @var Sender
+     */
+    private $sender;
+
+    /**
+     * @param Sender $sender
+     */
+    public function __construct(Sender $sender)
+    {
+        $this->sender = $sender;
+    }
+
+    /**
+     * @param PostResponseEvent $event
+     */
+    public function onTerminate(PostResponseEvent $event) {
+        $this->sender->send();
+    }
+}
---
+++ src/FML/Notification/NotificationBundle/FMLNotificationBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FML\Notification\NotificationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FMLNotificationBundle extends Bundle
+{
+}
