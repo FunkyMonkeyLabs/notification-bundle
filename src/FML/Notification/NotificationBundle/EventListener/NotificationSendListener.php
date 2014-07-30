50b7c1d865efb4232de344c421d4b1bfaff09cf7
Patryk Szlagowski <patryksz@lsnova.pl>
2014-07-30 12:15:30 +0200

    implements changes from notification lib

--- src/FML/Notification/NotificationBundle/EventListener/NotificationSendListener.php
+++ src/FML/Notification/NotificationBundle/EventListener/NotificationSendListener.php
@@ -2,31 +2,31 @@
 
 namespace FML\Notification\NotificationBundle\EventListener;
 
-use FML\Notification\Sender;
+use FML\Notification\MessageManager;
 use Symfony\Component\HttpKernel\Event\PostResponseEvent;
 
 /**
- * We are listening to terminate event and send notifications
+ * We are listening to terminate event and flush notifications
  */
 class NotificationSendListener {
 
     /**
-     * @var Sender
+     * @var MessageManager
      */
-    private $sender;
+    private $manager;
 
     /**
      * @param Sender $sender
      */
-    public function __construct(Sender $sender)
+    public function __construct(MessageManager $manager)
     {
-        $this->sender = $sender;
+        $this->manager = $manager;
     }
 
     /**
      * @param PostResponseEvent $event
      */
     public function onTerminate(PostResponseEvent $event) {
-        $this->sender->send();
+        $this->manager->flush();
     }
 }
